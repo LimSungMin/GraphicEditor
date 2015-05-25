7598d7fd274b16a87e1a7dc939105296db942984
임성민 <lsmorigin@gmail.com>
2015-05-25 16:11:30 +0900

    버튼 헤더파일 수정

--- GE/Resource.h
+++ GE/Resource.h
@@ -1,19 +1,20 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by GE.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// GE.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDD_GE_FORM			101
-#define IDR_MAINFRAME				128
-#define IDR_GETYPE				130
+#define IDD_ABOUTBOX                    100
+#define IDD_GE_FORM                     101
+#define IDR_MAINFRAME                   128
+#define IDR_GETYPE                      130
+#define IDC_BUTTON1                     1000
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif

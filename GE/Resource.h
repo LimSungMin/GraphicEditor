0565034d4c1f232982d312e0cb094bd3b432861a
임성민 <lsmorigin@gmail.com>
2015-05-28 00:48:17 +0900

    텍스트 라벨 추가

--- GE/Resource.h
+++ GE/Resource.h
@@ -7,6 +7,7 @@
 #define IDR_MAINFRAME                   128
 #define IDR_GETYPE                      130
 #define IDC_BUTTON1                     1000
+#define IDC_LIST1                       1001
 
 // Next default values for new objects
 // 
@@ -14,7 +15,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif

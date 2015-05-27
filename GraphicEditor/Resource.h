097d0ff95a3a570ee68e7ee7d84f533ede04a6d8
임성민 <lsmorigin@gmail.com>
2015-05-28 01:21:26 +0900

    여러 버튼 추가

--- GraphicEditor/Resource.h
+++ GraphicEditor/Resource.h
@@ -18,13 +18,17 @@
 #define ID_WINDOW_MANAGER               131
 #define IDS_EDIT_MENU                   306
 #define ID_BUTTON32772                  32772
+#define ID_BUTTON32774                  32774
+#define ID_BUTTON32775                  32775
+#define ID_BUTTON32777                  32777
+#define ID_BUTTON32781                  32781
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_COMMAND_VALUE         32782
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif

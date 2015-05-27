f6b1394504a9d98f80b9298d1ba25de2059a2842
임성민 <lsmorigin@gmail.com>
2015-05-28 01:10:50 +0900

    툴바에 원을 추가

--- GraphicEditor/Resource.h
+++ GraphicEditor/Resource.h
@@ -1,30 +1,31 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by GraphicEditor.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// GraphicEditor.rc에서 사용되고 있습니다.
 //
-#define IDD_ABOUTBOX				100
-#define IDD_GRAPHICEDITOR_FORM			101
-#define IDR_POPUP_EDIT				119
-#define ID_STATUSBAR_PANE1			120
-#define ID_STATUSBAR_PANE2			121
-#define IDS_STATUS_PANE1			122
-#define IDS_STATUS_PANE2			123
-#define IDS_TOOLBAR_STANDARD		124
-#define IDS_TOOLBAR_CUSTOMIZE		125
-#define ID_VIEW_CUSTOMIZE			126
-#define IDR_MAINFRAME				128
-#define IDR_MAINFRAME_256			129
-#define IDR_GraphicEditorTYPE				130
-#define ID_WINDOW_MANAGER			131
-#define IDS_EDIT_MENU				306
+#define IDD_ABOUTBOX                    100
+#define IDD_GRAPHICEDITOR_FORM          101
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDS_TOOLBAR_STANDARD            124
+#define IDS_TOOLBAR_CUSTOMIZE           125
+#define ID_VIEW_CUSTOMIZE               126
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_GraphicEditorTYPE           130
+#define ID_WINDOW_MANAGER               131
+#define IDS_EDIT_MENU                   306
+#define ID_BUTTON32772                  32772
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif

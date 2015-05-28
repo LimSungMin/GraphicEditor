1099751ab8f78f61794ffc7abc5326d3036ecac8
Keudeugi <black_stan@naver.com>
2015-05-28 13:11:57 +0900

    Point.cpp에 작성함

--- GraphicEditor/Point.h
+++ GraphicEditor/Point.h
@@ -14,22 +14,3 @@ private:
 	int x;
 	int y;
 };
-
-Point::Point() : x(0), y(0){}
-Point::Point(int x1, int y1) : x(x1), y(y1){}
-
-void Point::SetX(int x1){
-	x1 = x;
-}
-
-void Point::SetY(int y1){
-	y1 = y;
-}
-
-int Point::GetX(){ return x; }
-int Point::GetY(){ return y; }
-
-void Point::SetPoint(int x1, int y1){
-	x1 = x;
-	y1 = y;
-}

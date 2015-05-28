549e5a5be6d180e3975d6a200ef5c88136420e23
임성민 <lsmorigin@gmail.com>
2015-05-28 13:55:35 +0900

    Object 클래스 제거

--- GraphicEditor/Line.h
+++ GraphicEditor/Line.h
@@ -1,14 +1,13 @@
 #pragma once
-#include "Object.h"
-#include "Point.h"
-class Line :
-	public Point
+class Line	
 {
 public:
 	Line();
 	~Line();
 private:
+	int startx, starty;
 	int endx, endy;
+
 	BOOL toggle;
 };
 
--- GraphicEditor/Rectangle.h
+++ GraphicEditor/Rectangle.h
@@ -1,7 +1,6 @@
 #pragma once
-#include "Object.h"
-class Rectangle :
-	public Object
+
+class Rectangle 
 {
 public:
 	Rectangle();

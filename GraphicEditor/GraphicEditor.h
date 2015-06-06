9a2edf124a2de22490f49c06a43d29ae52df53f0
임성민 <lsmorigin@gmail.com>
2015-06-06 17:27:02 +0900

    리소스 수정

--- GraphicEditor/GraphicEditor.h
+++ GraphicEditor/GraphicEditor.h
@@ -8,6 +8,7 @@
 #endif
 
 #include "resource.h"       // 주 기호입니다.
+
 #include "GObject.h"
 #include "GRectangle.h"
 #include "GPolyline.h"
--- GraphicEditor/Resource.h
+++ GraphicEditor/Resource.h
@@ -24,6 +24,7 @@
 #define IDC_LineThick                   1005
 #define IDC_LinePattern                 1006
 #define IDC_COMBO2                      1008
+#define IDC_BUTTON1                     1009
 #define ID_ELLIPSE                      32772
 #define ID_RECTANGLE                    32775
 #define ID_TEXT                         32777
@@ -36,7 +37,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32783
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif

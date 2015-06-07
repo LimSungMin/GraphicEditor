152a977ee7aa340d26ec169cf5bab3ecbbd257e7
Keudeugi <black_stan@naver.com>
2015-06-08 00:22:27 +0900

    다중 선택 시도, for문으로 전체를 검사해 이동시키지만 하나만 이동함.

--- GraphicEditor/GraphicEditorView.h
+++ GraphicEditor/GraphicEditorView.h
@@ -37,6 +37,7 @@ public:
 	int m_changeSizePosition = -1;
 	int m_currentSelected; // 선택된 도형의 m_shape index
 	POINT m_clickedPoint; // 도형을 선택했을 때 클릭된 마우스 포인터의 좌표
+	GGroup m_group;
 
 	int polypointindex; // 선택된 사각형 임시 저장 좌표
 
